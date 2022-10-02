7e1527c50bda5ca542064484aa140c6a7761b68f
Лев Халяпин <lev.halyapin@yandex.ru>
2022-10-02 18:21:56 +0300

    Added IsVerticalWinUseCase

--- app/src/main/java/com/stdio/tictactoe/domain/usecases/IsVerticalWinUseCase.kt
+++ app/src/main/java/com/stdio/tictactoe/domain/usecases/IsVerticalWinUseCase.kt
@@ -3,6 +3,17 @@ package com.stdio.tictactoe.domain.usecases
 class IsVerticalWinUseCase {
 
     operator fun invoke(positions: List<Int>): Boolean {
-        TODO()
+        for (i in positions.indices) {
+            if (positionsCountWithStep3(i, positions) == 3) return true
+        }
+        return false
+    }
+
+    private fun positionsCountWithStep3(pos: Int, positions: List<Int>): Int {
+        var count = 0
+        for (i in pos until positions.size step 3) {
+            if (positions[i] == i) count++
+        }
+        return count
     }
 }
