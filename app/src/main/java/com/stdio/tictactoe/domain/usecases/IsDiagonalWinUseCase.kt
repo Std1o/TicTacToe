8b804960ec8f852f00b06ac1610b48b83f0c33f1
Alex <a.shmakov2708@yandex.ru>
2022-10-02 19:47:06 +0300

    Update IsDiagonalWinUseCase.kt
    Update IsDiagonalWinUseCase.kt

--- app/src/main/java/com/stdio/tictactoe/domain/usecases/IsDiagonalWinUseCase.kt
+++ app/src/main/java/com/stdio/tictactoe/domain/usecases/IsDiagonalWinUseCase.kt
@@ -3,6 +3,14 @@ package com.stdio.tictactoe.domain.usecases
 class IsDiagonalWinUseCase {
 
     operator fun invoke(positions: List<Int>): Boolean {
-        TODO()
+        val diagonalLine1 = listOf(0, 4, 8)
+        val diagonalLine2 = listOf(2, 4, 6)
+        val case1 = diagonalLine1.all {
+            positions.contains(it)
+        }
+        val case2 =  diagonalLine2.all {
+            positions.contains(it)
+        }
+        return case1 || case2
     }
 }
