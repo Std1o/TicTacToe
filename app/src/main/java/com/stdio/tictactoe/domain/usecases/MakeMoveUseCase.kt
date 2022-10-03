2a747fdab9700957fdabdb20eceb33089bd73976
Ilya <37378410+Std1o@users.noreply.github.com>
2022-10-03 20:40:46 +0300

    Update MakeMoveUseCase.kt

--- app/src/main/java/com/stdio/tictactoe/domain/usecases/MakeMoveUseCase.kt
+++ app/src/main/java/com/stdio/tictactoe/domain/usecases/MakeMoveUseCase.kt
@@ -1,10 +1,33 @@
 package com.stdio.tictactoe.domain.usecases
 
 import com.stdio.tictactoe.domain.GameState
+import com.stdio.tictactoe.domain.Player
 
 class MakeMoveUseCase {
 
+    private val isHorizontalWinUseCase = IsHorizontalWinUseCase()
+    private val isVerticalWinUseCase = IsVerticalWinUseCase()
+    private val isDiagonalWinUseCase = IsDiagonalWinUseCase()
+    private var currentPlayer: Player = Player.X
+    private val xPlayerMoves = MutableList(9) { -1 }
+    private val oPlayerMoves = MutableList(9) { -1 }
+
     operator fun invoke(position: Int): GameState {
-        TODO()
+        val positions = if (currentPlayer == Player.X) xPlayerMoves else oPlayerMoves
+        positions[position] = position
+        lateinit var gameState: GameState
+        if (isHorizontalWinUseCase(positions)) {
+            gameState = GameState.HorizontalWin(currentPlayer)
+        } else if (isVerticalWinUseCase(positions)) {
+            gameState = GameState.VerticalWin(currentPlayer)
+        } else if (isDiagonalWinUseCase(positions)) {
+            gameState = GameState.DiagonalWin(currentPlayer)
+        } else if ((xPlayerMoves.count { it != -1 } + oPlayerMoves.count { it != -1 }) == 9) {
+            gameState = GameState.Draw
+        } else {
+            gameState = GameState.Continue
+        }
+        currentPlayer = if (currentPlayer == Player.X) Player.O else Player.X
+        return gameState
     }
 }
