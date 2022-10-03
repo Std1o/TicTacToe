b2d058368ef10f131504cdd648d6053071d7251c
polinkasosiska <polina.sysoliatina@gmail.com>
2022-10-03 16:42:39 +0300

    IsHorizontalWinUseCase

--- app/src/main/java/com/stdio/tictactoe/domain/usecases/IsHorizontalWinUseCase.kt
+++ app/src/main/java/com/stdio/tictactoe/domain/usecases/IsHorizontalWinUseCase.kt
@@ -3,6 +3,12 @@ package com.stdio.tictactoe.domain.usecases
 class IsHorizontalWinUseCase {
 
     operator fun invoke(positions: List<Int>): Boolean {
-        TODO()
+        for (i in positions.indices step 3) {
+            val twoRunningInRow = 1 + positions[i] == positions[i + 1]
+            if (twoRunningInRow && (1 + positions[i + 1] == positions[i + 2])) {
+                return true
+            }
+        }
+        return false
     }
 }
